a7d5ab7467e3c0410301d46a2dbd6f00cc1a131a
Jerry Bendy <jerry@icewingcc.com>
2019-12-17 17:26:45 +0800

    Add tailing semicolons and change double-quote to single-quote

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
  */
 
 function touchX(event) {
-    if(event.type.indexOf("mouse") !== -1){
+    if(event.type.indexOf('mouse') !== -1){
         return event.clientX;
     }
     return event.touches[0].clientX;
 }
 
 function touchY(event) {
-    if(event.type.indexOf("mouse") !== -1){
+    if(event.type.indexOf('mouse') !== -1){
         return event.clientY;
     }
     return event.touches[0].clientY;
@@ -43,162 +43,163 @@ var vueTouchEvents = {
             touchHoldTolerance: 400,  // ms
             longTapTimeInterval: 400,  // ms
             touchClass: ''
-        }, options || {})
+        }, options || {});
 
 
         function touchStartEvent(event) {
             var $this = this.$$touchObj,
-                isTouchEvent = event.type.indexOf("touch") >= 0,
-                isMouseEvent = event.type.indexOf("mouse") >= 0,
-                $el = this
+                isTouchEvent = event.type.indexOf('touch') >= 0,
+                isMouseEvent = event.type.indexOf('mouse') >= 0,
+                $el = this;
 
             if (isTouchEvent) {
-                $this.lastTouchStartTime = event.timeStamp
+                $this.lastTouchStartTime = event.timeStamp;
             }
 
             if (isMouseEvent && $this.lastTouchStartTime && event.timeStamp - $this.lastTouchStartTime < 350) {
-                return
+                return;
             }
 
             if ($this.touchStarted) {
-                return
+                return;
             }
 
-            addTouchClass(this)
+            addTouchClass(this);
 
-            $this.touchStarted = true
+            $this.touchStarted = true;
 
-            $this.touchMoved = false
-            $this.swipeOutBounded = false
+            $this.touchMoved = false;
+            $this.swipeOutBounded = false;
 
-            $this.startX = touchX(event)
-            $this.startY = touchY(event)
+            $this.startX = touchX(event);
+            $this.startY = touchY(event);
 
-            $this.currentX = 0
-            $this.currentY = 0
+            $this.currentX = 0;
+            $this.currentY = 0;
 
-            $this.touchStartTime = event.timeStamp
+            $this.touchStartTime = event.timeStamp;
 
             // Trigger touchhold event after `touchHoldTolerance`ms
             $this.touchHoldTimer = setTimeout(function() {
-                triggerEvent(event, $el, 'touchhold')
-            }, options.touchHoldTolerance)
+                triggerEvent(event, $el, 'touchhold');
+            }, options.touchHoldTolerance);
 
-            triggerEvent(event, this, 'start')
+            triggerEvent(event, this, 'start');
         }
 
         function touchMoveEvent(event) {
-            var $this = this.$$touchObj
+            var $this = this.$$touchObj;
 
-            $this.currentX = touchX(event)
-            $this.currentY = touchY(event)
+            $this.currentX = touchX(event);
+            $this.currentY = touchY(event);
 
             if (!$this.touchMoved) {
-                var tapTolerance = options.tapTolerance
+                var tapTolerance = options.tapTolerance;
 
                 $this.touchMoved = Math.abs($this.startX - $this.currentX) > tapTolerance ||
-                    Math.abs($this.startY - $this.currentY) > tapTolerance
+                    Math.abs($this.startY - $this.currentY) > tapTolerance;
 
                 if($this.touchMoved){
-                    cancelTouchHoldTimer($this)
-                    triggerEvent(event, this, 'moved')
+                    cancelTouchHoldTimer($this);
+                    triggerEvent(event, this, 'moved');
                 }
 
             } else if (!$this.swipeOutBounded) {
-                var swipeOutBounded = options.swipeTolerance
+                var swipeOutBounded = options.swipeTolerance;
 
                 $this.swipeOutBounded = Math.abs($this.startX - $this.currentX) > swipeOutBounded &&
-                    Math.abs($this.startY - $this.currentY) > swipeOutBounded
+                    Math.abs($this.startY - $this.currentY) > swipeOutBounded;
             }
 
             if($this.touchMoved){
-                triggerEvent(event, this, 'moving')
+                triggerEvent(event, this, 'moving');
             }
         }
 
         function touchCancelEvent() {
-            var $this = this.$$touchObj
+            var $this = this.$$touchObj;
 
-            cancelTouchHoldTimer($this)
-            removeTouchClass(this)
+            cancelTouchHoldTimer($this);
+            removeTouchClass(this);
 
-            $this.touchStarted = $this.touchMoved = false
-            $this.startX = $this.startY = 0
+            $this.touchStarted = $this.touchMoved = false;
+            $this.startX = $this.startY = 0;
         }
 
         function touchEndEvent(event) {
             var $this = this.$$touchObj,
-                isTouchEvent = event.type.indexOf("touch") >= 0,
-                isMouseEvent = event.type.indexOf("mouse") >= 0
+                isTouchEvent = event.type.indexOf('touch') >= 0,
+                isMouseEvent = event.type.indexOf('mouse') >= 0;
 
             if (isTouchEvent) {
-                $this.lastTouchEndTime = event.timeStamp
+                $this.lastTouchEndTime = event.timeStamp;
             }
 
-            cancelTouchHoldTimer($this)
+            cancelTouchHoldTimer($this);
 
             if (isMouseEvent && $this.lastTouchEndTime && event.timeStamp - $this.lastTouchEndTime < 350) {
-                return
+                return;
             }
 
-            $this.touchStarted = false
+            $this.touchStarted = false;
 
-            removeTouchClass(this)
+            removeTouchClass(this);
 
             // Fix #33, Trigger `end` event when touch stopped
-            triggerEvent(event, this, 'end')
+            triggerEvent(event, this, 'end');
 
             if (!$this.touchMoved) {
                 // detect if this is a longTap event or not
                 if ($this.callbacks.longtap && event.timeStamp - $this.touchStartTime > options.longTapTimeInterval) {
-                    event.preventDefault()
-                    triggerEvent(event, this, 'longtap')
+                    event.preventDefault();
+                    triggerEvent(event, this, 'longtap');
 
                 } else {
                     // emit tap event
-                    triggerEvent(event, this, 'tap')
+                    triggerEvent(event, this, 'tap');
                 }
 
             } else if (!$this.swipeOutBounded) {
-                var swipeOutBounded = options.swipeTolerance, direction
+                var swipeOutBounded = options.swipeTolerance,
+                    direction;
 
                 if (Math.abs($this.startX - $this.currentX) < swipeOutBounded) {
-                    direction = $this.startY > $this.currentY ? "top" : "bottom"
+                    direction = $this.startY > $this.currentY ? 'top' : 'bottom';
 
                 } else {
-                    direction = $this.startX > $this.currentX ? "left" : "right"
+                    direction = $this.startX > $this.currentX ? 'left' : 'right';
                 }
 
                 // Only emit the specified event when it has modifiers
                 if ($this.callbacks['swipe.' + direction]) {
-                    triggerEvent(event, this, 'swipe.' + direction, direction)
+                    triggerEvent(event, this, 'swipe.' + direction, direction);
 
                 } else {
                     // Emit a common event when it has no any modifier
-                    triggerEvent(event, this, 'swipe', direction)
+                    triggerEvent(event, this, 'swipe', direction);
                 }
             }
         }
 
         function mouseEnterEvent() {
-            addTouchClass(this)
+            addTouchClass(this);
         }
 
         function mouseLeaveEvent() {
-            removeTouchClass(this)
+            removeTouchClass(this);
         }
 
         function triggerEvent(e, $el, eventType, param) {
-            var $this = $el.$$touchObj
+            var $this = $el.$$touchObj;
 
             // get the callback list
-            var callbacks = $this.callbacks[eventType] || []
+            var callbacks = $this.callbacks[eventType] || [];
             if (callbacks.length === 0) {
-                return null
+                return null;
             }
 
             for (var i = 0; i < callbacks.length; i++) {
-                var binding = callbacks[i]
+                var binding = callbacks[i];
 
                 if (binding.modifiers.stop) {
                     e.stopPropagation();
@@ -210,33 +211,33 @@ var vueTouchEvents = {
 
                 // handle `self` modifier`
                 if (binding.modifiers.self && e.target !== e.currentTarget) {
-                    continue
+                    continue;
                 }
 
                 if (typeof binding.value === 'function') {
                     if (param) {
-                        binding.value(param, e)
+                        binding.value(param, e);
                     } else {
-                        binding.value(e)
+                        binding.value(e);
                     }
                 }
             }
         }
 
         function addTouchClass($el) {
-            var className = $el.$$touchClass || options.touchClass
-            className && $el.classList.add(className)
+            var className = $el.$$touchClass || options.touchClass;
+            className && $el.classList.add(className);
         }
 
         function removeTouchClass($el) {
-            var className = $el.$$touchClass || options.touchClass
-            className && $el.classList.remove(className)
+            var className = $el.$$touchClass || options.touchClass;
+            className && $el.classList.remove(className);
         }
 
         function cancelTouchHoldTimer($this) {
             if ($this.touchHoldTimer) {
-                clearTimeout($this.touchHoldTimer)
-                $this.touchHoldTimer = null
+                clearTimeout($this.touchHoldTimer);
+                $this.touchHoldTimer = null;
             }
         }
 
@@ -249,98 +250,98 @@ var vueTouchEvents = {
                         callbacks: {},
                         // prevent bind twice, set to true when event bound
                         hasBindTouchEvents: false
-                    }
+                    };
 
 
                 // register callback
-                var eventType = binding.arg || 'tap'
+                var eventType = binding.arg || 'tap';
                 switch (eventType) {
                     case 'swipe':
-                        var _m = binding.modifiers
+                        var _m = binding.modifiers;
                         if (_m.left || _m.right || _m.top || _m.bottom) {
                             for (var i in binding.modifiers) {
                                 if (['left', 'right', 'top', 'bottom'].indexOf(i) >= 0) {
-                                    var _e = 'swipe.' + i
-                                    $el.$$touchObj.callbacks[_e] = $el.$$touchObj.callbacks[_e] || []
-                                    $el.$$touchObj.callbacks[_e].push(binding)
+                                    var _e = 'swipe.' + i;
+                                    $el.$$touchObj.callbacks[_e] = $el.$$touchObj.callbacks[_e] || [];
+                                    $el.$$touchObj.callbacks[_e].push(binding);
                                 }
                             }
                         } else {
-                            $el.$$touchObj.callbacks.swipe = $el.$$touchObj.callbacks.swipe || []
-                            $el.$$touchObj.callbacks.swipe.push(binding)
+                            $el.$$touchObj.callbacks.swipe = $el.$$touchObj.callbacks.swipe || [];
+                            $el.$$touchObj.callbacks.swipe.push(binding);
                         }
-                        break
+                        break;
 
                     default:
-                        $el.$$touchObj.callbacks[eventType] = $el.$$touchObj.callbacks[eventType] || []
-                        $el.$$touchObj.callbacks[eventType].push(binding)
+                        $el.$$touchObj.callbacks[eventType] = $el.$$touchObj.callbacks[eventType] || [];
+                        $el.$$touchObj.callbacks[eventType].push(binding);
                 }
 
                 // prevent bind twice
                 if ($el.$$touchObj.hasBindTouchEvents) {
-                    return
+                    return;
                 }
 
                 var passiveOpt = isPassiveSupported ? { passive: true } : false;
-                $el.addEventListener('touchstart', touchStartEvent, passiveOpt)
-                $el.addEventListener('touchmove', touchMoveEvent, passiveOpt)
-                $el.addEventListener('touchcancel', touchCancelEvent)
-                $el.addEventListener('touchend', touchEndEvent)
+                $el.addEventListener('touchstart', touchStartEvent, passiveOpt);
+                $el.addEventListener('touchmove', touchMoveEvent, passiveOpt);
+                $el.addEventListener('touchcancel', touchCancelEvent);
+                $el.addEventListener('touchend', touchEndEvent);
 
                 if (!options.disableClick) {
-                    $el.addEventListener('mousedown', touchStartEvent)
-                    $el.addEventListener('mousemove', touchMoveEvent)
-                    $el.addEventListener('mouseup', touchEndEvent)
-                    $el.addEventListener('mouseenter', mouseEnterEvent)
-                    $el.addEventListener('mouseleave', mouseLeaveEvent)
+                    $el.addEventListener('mousedown', touchStartEvent);
+                    $el.addEventListener('mousemove', touchMoveEvent);
+                    $el.addEventListener('mouseup', touchEndEvent);
+                    $el.addEventListener('mouseenter', mouseEnterEvent);
+                    $el.addEventListener('mouseleave', mouseLeaveEvent);
                 }
 
                 // set bind mark to true
-                $el.$$touchObj.hasBindTouchEvents = true
+                $el.$$touchObj.hasBindTouchEvents = true;
             },
 
             unbind: function ($el) {
-                $el.removeEventListener('touchstart', touchStartEvent)
-                $el.removeEventListener('touchmove', touchMoveEvent)
-                $el.removeEventListener('touchcancel', touchCancelEvent)
-                $el.removeEventListener('touchend', touchEndEvent)
+                $el.removeEventListener('touchstart', touchStartEvent);
+                $el.removeEventListener('touchmove', touchMoveEvent);
+                $el.removeEventListener('touchcancel', touchCancelEvent);
+                $el.removeEventListener('touchend', touchEndEvent);
 
                 if (!options.disableClick) {
-                    $el.removeEventListener('mousedown', touchStartEvent)
-                    $el.removeEventListener('mousemove', touchMoveEvent)
-                    $el.removeEventListener('mouseup', touchEndEvent)
-                    $el.removeEventListener('mouseenter', mouseEnterEvent)
-                    $el.removeEventListener('mouseleave', mouseLeaveEvent)
+                    $el.removeEventListener('mousedown', touchStartEvent);
+                    $el.removeEventListener('mousemove', touchMoveEvent);
+                    $el.removeEventListener('mouseup', touchEndEvent);
+                    $el.removeEventListener('mouseenter', mouseEnterEvent);
+                    $el.removeEventListener('mouseleave', mouseLeaveEvent);
                 }
 
                 // remove vars
-                delete $el.$$touchObj
+                delete $el.$$touchObj;
             }
-        })
+        });
 
         Vue.directive('touch-class', {
             bind: function ($el, binding) {
-                $el.$$touchClass = binding.value
+                $el.$$touchClass = binding.value;
             },
             unbind: function ($el) {
-                delete $el.$$touchClass
+                delete $el.$$touchClass;
             }
-        })
+        });
     }
-}
+};
 
 
 /*
  * Exports
  */
 if (typeof module === 'object') {
-    module.exports = vueTouchEvents
+    module.exports = vueTouchEvents;
 
 } else if (typeof define === 'function' && define.amd) {
     define([], function () {
-        return vueTouchEvents
-    })
+        return vueTouchEvents;
+    });
 } else if (window.Vue) {
-    window.vueTouchEvents = vueTouchEvents
-    Vue.use(vueTouchEvents)
+    window.vueTouchEvents = vueTouchEvents;
+    Vue.use(vueTouchEvents);
 }
